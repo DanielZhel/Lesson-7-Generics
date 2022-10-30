d0c7ae7b0365878ab93df61ab1cf8b38705759f3
Danielzhel <fartunazh@gmail.com>
2022-10-30 21:33:05 +0300

    Добавьте файлы проекта.

---
+++ Lesson 8 Generics/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_8_Generics
+{
+    public class Pair<S,T>
+    {
+        public T Password { get; }
+        public S Id { get; }
+
+        public Pair(T password, S id)
+        {
+            Password = password;
+            Id = id;
+        }
+    }
+}
---
+++ Lesson 8 Generics/Program.cs
@@ -0,0 +1,12 @@
+﻿using Lesson_8_Generics;
+
+Console.WriteLine("Enter password.");
+string password = Console.ReadLine();
+
+Console.WriteLine("Enter Id");
+int id = Convert.ToInt16(Console.ReadLine());
+
+Pair<int, string> User = new Pair<int, string>(password, id);
+Console.WriteLine(User.Password);
+Console.WriteLine(User.Id);
+
